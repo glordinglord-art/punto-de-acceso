3460c40673199aeac6fbe1ccc96879abfc8e6f4a
grendleard <grendleard@gmail.com>
2026-05-11 02:12:16 -0500

    feat: refactor PushSubscriptionDto to use class-validator for improved validation

--- olympus-bite-bk/src/modules/notifications/application/dtos/notification.dto.ts
+++ olympus-bite-bk/src/modules/notifications/application/dtos/notification.dto.ts
@@ -1,22 +1,73 @@
-export interface PushSubscriptionDto {
-  endpoint: string;
-  keys: {
-    p256dh: string;
-    auth: string;
-  };
+import { Type } from 'class-transformer';
+import {
+  IsBoolean,
+  IsInt,
+  IsNotEmpty,
+  IsObject,
+  IsOptional,
+  IsString,
+  Max,
+  Min,
+  ValidateNested,
+} from 'class-validator';
+
+export class PushSubscriptionKeysDto {
+  @IsString()
+  @IsNotEmpty()
+  p256dh!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  auth!: string;
+}
+
+export class PushSubscriptionDto {
+  @IsString()
+  @IsNotEmpty()
+  endpoint!: string;
+
+  @IsObject()
+  @ValidateNested()
+  @Type(() => PushSubscriptionKeysDto)
+  keys!: PushSubscriptionKeysDto;
 }
 
 export class SaveSubscriptionDto {
+  @ValidateNested()
+  @Type(() => PushSubscriptionDto)
   subscription!: PushSubscriptionDto;
+
+  @IsOptional()
+  @IsString()
   userAgent?: string;
 }
 
 export class UpdateNotificationPreferencesDto {
+  @IsOptional()
+  @IsBoolean()
   enabled?: boolean;
+
+  @IsOptional()
+  @IsString()
   breakfastTime?: string;
+
+  @IsOptional()
+  @IsString()
   lunchTime?: string;
+
+  @IsOptional()
+  @IsString()
   dinnerTime?: string;
+
+  @IsOptional()
+  @IsString()
   workoutTime?: string;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(-840)
+  @Max(840)
   timezoneOffset?: number;
 }
 
